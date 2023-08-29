620d87f30a1b0d4d84082393f0ebc4e17ceb6e00
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-08-29 20:16:06 +0100

    feat: initial stab

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
-# TODO: insert resources here.
+resource "azurerm_linux_virtual_machine_scale_set" {
+  for_each            = var.os_type == "linux" ? toset(["this"]) : toset([])
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  admin_username      = var.admin_username
+  sku                 = var.sku
+  instances           = var.instances
+}
+
+resource "azurerm_windows_virtual_machine_scale_set" {
+  for_each            = var.os_type == "windows" ? toset(["this"]) : toset([])
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  admin_username      = var.admin_username
+  sku                 = var.sku
+  instances           = var.instances
+}
--- variables.tf
+++ variables.tf
@@ -8,8 +8,48 @@ If it is set to false, then no telemetry will be collected.
 DESCRIPTION
 }
 
-# This is required for most resource modules
 variable "resource_group_name" {
   type        = string
   description = "The resource group where the resources will be deployed."
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure location where the resources will be deployed."
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "A map of tags to add to the VM scale set."
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the VM scale set."
+}
+
+variable "os_type" {
+  type        = string
+  description = "The type of operating system, either `linux` or `windows`."
+  validation {
+    condition     = can(regex("^linux$|^windows$", var.os_type))
+    error_message = "The os_type variable must be either linux or windows."
+  }
+
+  variable "sku" {
+    type        = string
+    description = "The VM SKU to use for the VM scale set, e.g. `Standard_F2`."
+  }
+
+  variable "instances" {
+    type        = number
+    description = "The number of instances to create in the VM scale set."
+  }
+
+  variable "admin_password" {
+    type        = string
+    description = "The admin password for the VM scale set. Conflicts with admin_ssh_key."
+    sensitive   = true
+    default     = null
+  }
 }
