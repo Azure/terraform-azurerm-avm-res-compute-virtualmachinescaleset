60d38e47aa496025a00d0d344c9c544fd6f5482b
terrymandin <terry.mandin@microsoft.com>
2024-02-05 13:38:12 -0700

    Set advanced example to zone redundancy

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -157,7 +157,7 @@ module "terraform_azurerm_avm_res_compute_virtualmachinescaleset" {
   eviction_policy = "Deallocate"
   # Instance Placement
   zone_balance                 = false
-  zones                        = ["1"]
+  zones                        = ["1", "2", "3"]
   proximity_placement_group_id = azurerm_proximity_placement_group.this.id
   single_placement_group       = false
   # Miscellanous settings
