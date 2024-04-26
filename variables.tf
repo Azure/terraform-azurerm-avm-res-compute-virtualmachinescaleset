0064bfbaede16c6e199914fff5aa17f9dca07140
terrymandin <terry.mandin@microsoft.com>
2024-04-26 13:31:31 -0600

    linting fix

--- variables.tf
+++ variables.tf
@@ -697,24 +697,6 @@ variable "role_assignments" {
   > Note: only set `skip_service_principal_aad_check` to true if you are assigning a role to a service principal.
   DESCRIPTION
   }
-  
-  locals {
-    role_definition_resource_substring = "providers/Microsoft.Authorization/roleDefinitions"
-  }
-  
-  # Example resource declaration
-  resource "azurerm_role_assignment" "this" {
-    for_each                               = var.role_assignments
-    scope                                  = azurerm_MY_RESOURCE.this.id
-    role_definition_id                     = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? each.value.role_definition_id_or_name : null
-    role_definition_name                   = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? null : each.value.role_definition_id_or_name
-    principal_id                           = each.value.principal_id
-    condition                              = each.value.condition
-    condition_version                      = each.value.condition_version
-    skip_service_principal_aad_check       = each.value.skip_service_principal_aad_check
-    delegated_managed_identity_resource_id = each.value.delegated_managed_identity_resource_id
-    principal_type                         = each.value.principal_type
-  }
 
 variable "single_placement_group" {
   type        = bool
