08cb18956a1b69083bca6933c13a34c5b6e15a68
terrymandin <terry.mandin@microsoft.com>
2024-04-26 13:28:12 -0600

    Fix linting errors

--- variables.tf
+++ variables.tf
@@ -480,7 +480,7 @@ variable "os_profile" {
       admin_username                  = string
       computer_name_prefix            = optional(string)
       disable_password_authentication = optional(bool)
-      patch_assessment_mode           = optional(string) //, "AutomaticByPlatform")
+      patch_assessment_mode           = optional(string) 
       patch_mode                      = optional(string, "AutomaticByPlatform")
       provision_vm_agent              = optional(bool, true)
       admin_ssh_key_id                = optional(set(string))
@@ -496,7 +496,7 @@ variable "os_profile" {
       computer_name_prefix     = optional(string)
       enable_automatic_updates = optional(bool, true)
       hotpatching_enabled      = optional(bool)
-      patch_assessment_mode    = optional(string) //, "AutomaticByPlatform")
+      patch_assessment_mode    = optional(string) 
       patch_mode               = optional(string, "AutomaticByPlatform")
       provision_vm_agent       = optional(bool, true)
       timezone                 = optional(string)
@@ -670,29 +670,51 @@ variable "proximity_placement_group_id" {
 }
 
 variable "role_assignments" {
-  type = map(object({
-    role_definition_id_or_name             = string
-    principal_id                           = string
-    description                            = optional(string, null)
-    skip_service_principal_aad_check       = optional(bool, false)
-    condition                              = optional(string, null)
-    condition_version                      = optional(string, null)
-    delegated_managed_identity_resource_id = optional(string, null)
-  }))
-  default     = {}
-  description = <<DESCRIPTION
-  A map of role assignments to create on the Virtual Machine Scale Set. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+    type = map(object({
+      role_definition_id_or_name             = string
+      principal_id                           = string
+      description                            = optional(string, null)
+      skip_service_principal_aad_check       = optional(bool, false)
+      condition                              = optional(string, null)
+      condition_version                      = optional(string, null)
+      delegated_managed_identity_resource_id = optional(string, null)
+      principal_type                         = optional(string, null)
+    }))
+    default     = {}
+    nullable    = false
+    description = <<DESCRIPTION
+  A map of role assignments to create on the <RESOURCE>. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
   
   - `role_definition_id_or_name` - The ID or name of the role definition to assign to the principal.
   - `principal_id` - The ID of the principal to assign the role to.
-  - `description` - The description of the role assignment.
-  - `skip_service_principal_aad_check` - If set to true, skips the Azure Active Directory check for the service principal in the tenant. Defaults to false.
-  - `condition` - The condition which will be used to scope the role assignment.
-  - `condition_version` - The version of the condition syntax. Leave as `null` if you are not using a condition, if you are then valid values are '2.0'.
+  - `description` - (Optional) The description of the role assignment.
+  - `skip_service_principal_aad_check` - (Optional) If set to true, skips the Azure Active Directory check for the service principal in the tenant. Defaults to false.
+  - `condition` - (Optional) The condition which will be used to scope the role assignment.
+  - `condition_version` - (Optional) The version of the condition syntax. Leave as `null` if you are not using a condition, if you are then valid values are '2.0'.
+  - `delegated_managed_identity_resource_id` - (Optional) The delegated Azure Resource Id which contains a Managed Identity. Changing this forces a new resource to be created. This field is only used in cross-tenant scenario.
+  - `principal_type` - (Optional) The type of the `principal_id`. Possible values are `User`, `Group` and `ServicePrincipal`. It is necessary to explicitly set this attribute when creating role assignments if the principal creating the assignment is constrained by ABAC rules that filters on the PrincipalType attribute.
   
   > Note: only set `skip_service_principal_aad_check` to true if you are assigning a role to a service principal.
   DESCRIPTION
-}
+  }
+  
+  locals {
+    role_definition_resource_substring = "providers/Microsoft.Authorization/roleDefinitions"
+  }
+  
+  # Example resource declaration
+  resource "azurerm_role_assignment" "this" {
+    for_each                               = var.role_assignments
+    scope                                  = azurerm_MY_RESOURCE.this.id
+    role_definition_id                     = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? each.value.role_definition_id_or_name : null
+    role_definition_name                   = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? null : each.value.role_definition_id_or_name
+    principal_id                           = each.value.principal_id
+    condition                              = each.value.condition
+    condition_version                      = each.value.condition_version
+    skip_service_principal_aad_check       = each.value.skip_service_principal_aad_check
+    delegated_managed_identity_resource_id = each.value.delegated_managed_identity_resource_id
+    principal_type                         = each.value.principal_type
+  }
 
 variable "single_placement_group" {
   type        = bool
@@ -738,9 +760,9 @@ EOT
 
 # AVM interface variables
 variable "tags" {
-  type        = map(any)
-  default     = null
-  description = "Map of tags to assign to the resources. "
+    type     = map(string)
+    default  = null
+    description = "(Optional) Tags of the resource."
 }
 
 variable "termination_notification" {
