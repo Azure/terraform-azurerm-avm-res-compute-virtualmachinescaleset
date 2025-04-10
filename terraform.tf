4c84830e43eca62bcb0e3fa3c933382589a1d086
Tobias Lindberg <tobias.ehlert@gmail.com>
2025-04-10 12:59:44 +0200

    fix: use new azurerm vmss inputs

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,9 @@
 terraform {
   required_version = ">= 1.9, < 2.0"
   required_providers {
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 2.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 4.26"
     }
     modtm = {
       source  = "Azure/modtm"
