681230b388d739b750a7baa714b5bbdea3c2d07c
Min Hein Aung <minheinaung97@gmail.com>
2025-08-01 00:17:09 +0800

    fix inconsistent final plan error caused by latest azurerm version and fix e2e pipeline error (#141)
    * fix inconsistent final plan error caused by latest azurerm version and fix e2e pipeline error
    
    * edit azurerm version constraints to pass pr check

--- terraform.tf
+++ terraform.tf
@@ -8,7 +8,8 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.26"
+      version = "~> 4.26, != 4.37.0"
+      # version 4.37.0 introduced a new variable network_api_version causes inconsistent final plan error, this is probably due to a bug in azurerm go code DiffSuppressFunc for network_api_version. 
     }
     modtm = {
       source  = "Azure/modtm"
