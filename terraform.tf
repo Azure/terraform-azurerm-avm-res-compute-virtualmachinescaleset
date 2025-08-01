93c38513f0c2bbe618c0b555dfa6ea2538265c31
Min Hein Aung <minheinaung97@gmail.com>
2025-08-01 22:00:29 +0800

    adds azurerm version constraints to newly released 4.38.0 and 4.38.1 (#142)
    * fix inconsistent final plan error caused by latest azurerm version and fix e2e pipeline error
    
    * edit azurerm version constraints to pass pr check
    
    * update azurerm constraints to exclude newly released 4.38.0 and 4.38.1

--- terraform.tf
+++ terraform.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.26, != 4.37.0"
+      version = "~> 4.26, != 4.37.0, != 4.38.0, != 4.38.1"
       # version 4.37.0 introduced a new variable network_api_version causes inconsistent final plan error, this is probably due to a bug in azurerm go code DiffSuppressFunc for network_api_version. 
     }
     modtm = {
